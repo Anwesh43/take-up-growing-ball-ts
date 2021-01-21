95bd7753e1b8c250237e251b43570d2f0b466e6a
Anwesh43 <anweshthecool0@gmail.com>
2021-01-21 07:21:42 +0530

    adding TUGBNode class

--- index.ts
+++ index.ts
@@ -143,3 +143,45 @@ class Animator {
         }
     }
 }
+
+class TUGBNode {
+    
+    prev : TUGBNode 
+    next : TUGBNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new TUGBNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+    
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawTUGBNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : TUGBNode {
+        var curr : TUGBNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
