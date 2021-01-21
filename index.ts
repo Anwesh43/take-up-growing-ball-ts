c65aa23e238a0c20dfbe3db2910ba479e37a8cff
Anwesh43 <anweshthecool0@gmail.com>
2021-01-21 07:07:13 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 const w : number = window.innerWidth 
 const h : number = window.innerHeight
-const parts : number = 3  
+const parts : number = 4 
 const scGap : number = 0.02 / parts  
 const balls : number = 6
 const strokeFactor : number = 90 
@@ -26,5 +26,45 @@ class ScaleUtil {
 
     static sinify(scale : number) : number {
         return Math.sin(scale * Math.PI)
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawCircle(context : CanvasRenderingContext2D, x : number, y : number, r : number) {
+        context.beginPath()
+        context.arc(x, y, r, 0, 2 * Math.PI)
+        context.fill()
+    }
+
+    static drawTakeUpGrowingBall(context : CanvasRenderingContext2D, scale : number) {
+        const gap : number = w / (2 * balls + 1)
+        const r : number = gap / 2 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        for (var j = 0; j < balls; j++) {
+            context.save()
+            context.translate(gap / 2 + gap * j, 0)
+            DrawingUtil.drawCircle(context, 0,  r + (h - 2 * r) * (1 - sc3), r * (sc1 - sc4))
+            DrawingUtil.drawLine(context, 0, 0, 0, (h - 2 * r) * (sc2 - sc3))
+            context.restore()
+        }
+    }
+
+    static drawTUGBNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.fillStyle = colors[i]
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawTakeUpGrowingBall(context, scale)
     }
 }
